7bc2bda426193f9a3c98074bf0e3bd9dd38e7188
sudatra <mail2sudatra@gmail.com>
2025-09-04 20:00:50 +0530

    added go git package for checking commits stats

--- go-cli-git-commits/stats/stats.go
+++ go-cli-git-commits/stats/stats.go
@@ -1,11 +1,12 @@
 package stats
 
 import (
-	"fmt"
 	"github.com/sudatra/go-cli-git-commits/scanner"
+	"github.com/go-git/go-git/v5"
 )
 
 const daysInLastSixMonths = 183;
+const outOfRange = 99999
 
 func processRepositories(email string) map[int]int {
 	filePath := scanner.GetDotFilePath();
@@ -27,4 +28,42 @@ func processRepositories(email string) map[int]int {
 func Stats(email string) {
 	commits := processRepositories(email);
 	printCommitStats(commits);
+}
+
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+	repo, err := git.PlainOpen(path);
+	if err != nil {
+		panic(err);
+	}
+
+	ref, err := repo.Head();
+	if err != nil {
+		panic(err);
+	}
+
+	iterator,err := repo.Log(&git.LogOptions{From: ref.Hash()});
+	if err != nil {
+		panic(err);
+	}
+
+	offset := calcOffset();
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset;
+
+		if c.Author.Email != email {
+			return nil;
+		}
+
+		if daysAgo != outOfRange {
+			commits[daysAgo]++;
+		}
+
+		return nil;
+	})
+
+	if err != nil {
+		panic(err);
+	}
+
+	return commits;
 }
