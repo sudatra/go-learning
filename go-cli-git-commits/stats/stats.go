cc64aada648972062dccc38f5e1e1ade757fd23c
sudatra <mail2sudatra@gmail.com>
2025-09-06 12:15:02 +0530

    added individual cell print

--- go-cli-git-commits/stats/stats.go
+++ go-cli-git-commits/stats/stats.go
@@ -6,11 +6,14 @@ import (
 	"time"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/sudatra/go-cli-git-commits/scanner"
 )
 
 const daysInLastSixMonths = 183;
 const outOfRange = 99999;
+const weeksInLastSixMonths = 26;
+
 type column []int;
 
 func processRepositories(email string) map[int]int {
@@ -129,7 +132,7 @@ func printCommitStats(commits map[int]int) {
 
 func sortMapIntoSlice(mp map[int]int) []int {
 	var keys []int;
-	for k := range m {
+	for k := range mp {
 		keys = append(keys, k);
 	}
 
@@ -169,7 +172,7 @@ func printCells(cols map[int]column) {
 
 			if col, ok := cols[i]; ok {
 				if i == 0 && j == calcOffset() - 1 {
-					printCell(cols[j], true);
+					printCell(col[j], true);
 					continue;
 				} else {
 					if len(col) > j {
@@ -198,4 +201,59 @@ func printDayCol(day int) {
 	}
 
 	fmt.Printf(out);
+}
+
+func printMonths() {
+	week := getBeginningOfDay(time.Now().Add(-(daysInLastSixMonths * time.Hour * 24)));
+	month := week.Month();
+
+	fmt.Printf("         ");
+
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3]);
+			month = week.Month();
+		} else {
+			fmt.Printf("		");
+		}
+
+		week = week.Add(7 * time.Hour * 24);
+		if week.After(time.Now()) {
+			break;
+		}
+	}
+
+	fmt.Printf("\n");
+}
+
+func printCell(val int, today bool) {
+	escape := "\033[0;37;30m";
+
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m";
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;43m";
+	case val >= 10:
+		escape = "\033[1;30;42m";
+	}
+
+	if today {
+		escape = "\033[1;37;45m";
+	}
+
+	if val == 0 {
+		fmt.Printf(escape + "  - " + "\033[0m");
+		return;
+	}
+
+	str := "  %d ";
+	switch {
+	case val >= 10:
+		str = " %d ";
+	case val >= 100:
+		str = "%d ";
+	}
+
+	fmt.Printf(escape+str+"\033[0m", val);
 }
